6a0d94ee50bda07839e493b58a0e9db0f0583de3
Mar <mar@strawmelonjuice.com>
2024-03-22 00:40:28 +0100

    Update web.js

--- web.js
+++ web.js
@@ -1,29 +1,31 @@
-{function LoadImgMote(currimgmote) {
-    function retrieve(url, cb) {
-        $.get(url, (data) => {
-            cb(data);
-        });
-    }
-    let name = (currimgmote.innerHTML).replace(":", "_");
-    retrieve(`/es/imgmotes/i/${name}`, (response) => {
-        let newimgmote = document.createElement("img");
-        newimgmote.setAttribute("style", "max-width: 16px; max-height: 16px");
-        newimgmote.setAttribute("loading", "lazy");
-        newimgmote.setAttribute("alt", ":" + name + ":-imgmote.");
-        newimgmote.classList.add("imgmote");
-        if (response == "Not Found") {
-            console.warn(`IMGMOTES: Could not find the imgmote '${name}'!`)
-        }
-        newimgmote.src = response;
-        currimgmote.parentNode.replaceChild(newimgmote, currimgmote);
-    });
-}
-    
-    setInterval(() => {
-        let imgmotes = (document.getElementsByTagName("imgmote"))
-        ;
-        for (let i = 0; i < imgmotes.length; i++) {
-            LoadImgMote(imgmotes.item(i));
-        }
-    }, 100);
+{
+	function LoadImgMote(currimgmote) {
+		function retrieve(url, cb) {
+			$.get(url, (data) => {
+				cb(data);
+			});
+		}
+		const name = currimgmote.innerHTML.replace(":", "_");
+		retrieve(`/es/imgmotes/i/${name}`, (response) => {
+			const newimgmote = document.createElement("img");
+			newimgmote.setAttribute("style", "max-width: 16px; max-height: 16px");
+			newimgmote.setAttribute("loading", "lazy");
+			newimgmote.setAttribute("alt", `:${name}:-imgmote.`);
+			newimgmote.classList.add("imgmote");
+			if (response === "Not Found") {
+				console.warn(`IMGMOTES: Could not find the imgmote '${name}'!`);
+			}
+			newimgmote.src = response;
+			currimgmote.parentNode.replaceChild(newimgmote, currimgmote);
+		});
+	}
+
+	setInterval(() => {
+		const imgmotes = document.getElementsByTagName("imgmote");
+		if (imgmotes.length > 0) {
+			for (let i = 0; i < imgmotes.length; i++) {
+				LoadImgMote(imgmotes.item(i));
+			}
+		}
+	}, 100);
 }
